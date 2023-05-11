98a1c056bceb7371c5c9186e119c46bef7d71ae0
ziogref <ziogref@users.noreply.github.com>
2023-05-11 06:37:27 +0000

    Updated to account for the weekly changing Coles URL

--- PepsiMaxHunter.py
+++ PepsiMaxHunter.py
@@ -1,4 +1,4 @@
-import requests, http.client, urllib, re
+import requests, http.client, urllib, re, datetime, pytz, math
 
 #Product links in JSON format
 links = {
@@ -8,6 +8,7 @@ links = {
 'WooliesPepsi10Link': 'https://www.woolworths.com.au/shop/productdetails/442190/pepsi-max-cans',
 'WooliesPepsi24Link': 'https://www.woolworths.com.au/shop/productdetails/54291/pepsi-max-cans',
 'WooliesPepsi30Link': 'https://www.woolworths.com.au/shop/productdetails/773129/pepsi-max-cans',
+'kirks': 'https://www.coles.com.au/product/kirks-zero-sugar-orange-soft-drink-cans-10x375ml-10-pack-6421566'
 }
 
 # User Agent was pulled from my Dell Laptop on 04/05/2023
@@ -20,6 +21,40 @@ ProcessedInformation = {}
 response = requests.get('https://www.woolworths.com.au', headers=getHeaders, timeout=10)
 cookies = response.cookies
 WooliesAPICookie = {'bm_sz': cookies.get('bm_sz')}
+
+# Lets get Wednesday!! (for coles)
+# Gets current date in current timezone
+CurrentDate = datetime.datetime.now()
+
+# Converts date to what it is currently in UTC 0 
+UTC0Date = CurrentDate.astimezone(pytz.timezone("UTC"))
+
+# Gets the day of the week. 0 is Monday, 1 is Tuesday etc) 
+DayOfWeek = datetime.date.weekday(UTC0Date)
+
+# Lets do math. To get the day of the week, we need to subtract Wednesdays value (2) from the current day.
+OffsetDay = (DayOfWeek -2) % 7
+
+# TNow we subtract the offset from todays date (DayOfWeek) to get us the most recent Wednesday
+WednesdayDateTime = UTC0Date - datetime.timedelta(days=OffsetDay)
+
+# Strips all extra info and puts date into yyyymmdd format
+WednesdayDate = WednesdayDateTime.strftime("%Y%m%d")
+#End of Wednesday Finder
+
+# Lets find the coles Version Number!
+# Suspected start date of Coles v3.00
+Colesv300 = datetime.date(2022, 9, 7)
+
+# Gets Todays Date
+Today = datetime.date.today()
+
+# Gets the amount of weeks since the Colesv3.00 start date
+VersionNumber = math.floor((Today - Colesv300).days / 7)
+# End of getting Coles Version Number
+
+# Form coles URL format
+ColesURL = "https://www.coles.com.au/_next/data/" + WednesdayDate + ".01_v3." + str(VersionNumber) + ".0/en/product/"
 
 # for loop that goes over each item in links array
 # link is the link name (e.g ColesPepsi24) | links.keys is items within the array
@@ -43,7 +78,7 @@ for link in links.keys():
             product_code = url_parts[-1]
 
             # Add the Product code to the API URL
-            apilink = "https://www.coles.com.au/_next/data/20230502.01_v3.34.0/en/product/" + product_code + ".json"           
+            apilink = ColesURL + product_code + ".json"         
 
             # Gets the data from the Coles links
             jsondata = requests.get(apilink, headers=getHeaders, timeout=10).json()
